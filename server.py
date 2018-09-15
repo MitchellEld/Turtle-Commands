26b388811d016648af14ab7262cbc4f1d7b91b9e
Thomas McDonald <thomasmcdonald@Thomass-MacBook-Pro.local>
2018-09-15 17:49:51 -0500

    Added number verification

--- server.py
+++ server.py
@@ -4,10 +4,23 @@ from twilio.twiml.messaging_response import Body, Message, MessagingResponse
 
 app = Flask(__name__)
 
+verified_numbers = []
+
+with open("./setup/verified_numbers", "r") as phone_nums:
+    verified_numbers = phone_nums.readlines()
+
 @app.route("/", methods=['POST'])
 def get_message():
     print(request.form['Body'])
+    from_number = request.form['From']
+    req_body = request.form['Body']
+    response = MessagingResponse()
+    message = Message()
     # Verify that phone number is able to make requests
+    if from_number not in verified_numbers:
+        message.body("You are not authorized")
+        response.append(message)
+        return str(response)
 
     # Check first word in body to see if it is custom command
 
@@ -18,8 +31,7 @@ def get_message():
     # Capture output of the command
 
     # return message with the output
-    response = MessagingResponse()
-    message = Message()
+    
     message.body("Got the request")
     response.append(message)
     return str(response)
