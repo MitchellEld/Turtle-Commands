eb7073dd4248908deab5171cbf2393de060a7403
enochnic <enochnic@tamu.edu>
2018-09-15 23:41:02 -0500

    switched to slack integration instead of text

--- server.py
+++ server.py
@@ -1,5 +1,4 @@
 from flask import Flask, request
-from twilio.twiml.messaging_response import Body, Message, MessagingResponse
 from utils import get_commands, execute_commands
 
 app = Flask(__name__)
@@ -8,15 +7,10 @@ app = Flask(__name__)
 def get_message():
     # from_number = request.form['From']
     req_body = request.form['Body']
-    response = MessagingResponse()
-    message = Message()
 
     # Check first word in body to see if it is custom command
     commands = get_commands(req_body)
     # If custom command, run function associated with it
     # If not custom command, run the body as normal os command
     output = execute_commands(commands)
-
-    message.body('Command output: \n{}'.format(output))
-    response.append(message)
-    return str(response)
+    return output
---
+++ slack.py
@@ -0,0 +1,23 @@
+from slackeventsapi import SlackEventAdapter
+from slackclient import SlackClient
+import os
+import requests
+
+
+url = "http://127.0.0.1:5000"
+
+# Our app's Slack Event Adapter for receiving actions via the Events API
+slack_signing_secret = 'da82529a03ba7e478e3ee2770bfa7dce'
+slack_events_adapter = SlackEventAdapter(slack_signing_secret, "/slack/events")
+
+slack_client = SlackClient('xoxb-435732900096-436461570514-afavU32dRQt8aT98iSP2HPQj')
+
+@slack_events_adapter.on("message")
+def handle_message(event_data):
+    #print(event_data)
+    if "user" in event_data["event"]:
+        r = requests.post(url, data = {'Body': event_data['event']['text']})
+        slack_client.api_call("chat.postMessage", channel=event_data['event']["channel"], text=r.text)
+
+
+slack_events_adapter.start(port=3000)
